b7b0a850ed874982f5effa580af1c7516daaeff5
rest0509 <re0509@hotmail.com>
2015-04-08 15:17:28 -0700

    suma if block

---
+++ adivino.rb
@@ -0,0 +1,10 @@
+puts 'yo soy un adivino. Dime tu nombre'
+nombre = gets.chomp
+if nombre == 'chris'
+puts 'veo grandes cosas en su futuro'
+elsif nombre == 'carlos'
+puts 'veo una grande ganancia en su futuro'
+else
+puts 'veo en tu futuro... oh oh! Es tarde'
+puts 'losiento, pero tengo que salir corriendo!'
+end
